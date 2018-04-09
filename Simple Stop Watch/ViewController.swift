39185c26ad7a2ccffb551a09f4db27973de3f620
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-09 15:31:11 +0900

    Initial Commit

---
+++ Simple Stop Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Stop Watch
+//
+//  Created by 김종현 on 2018. 4. 9..
+//  Copyright © 2018년 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
